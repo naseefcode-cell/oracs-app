2ad8435cfe4d79bc9a600ef787f6edaf58d15496
naseefcode-cell <naseef.code@gmail.com>
2025-11-21 17:06:47 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,4 +1,6 @@
-const API_BASE = 'http://localhost:5000/api';
+
+const API_BASE = 'https://www.oracs.in/api';
+
 
 // Enhanced State Management
 let currentUser = JSON.parse(localStorage.getItem('currentUser')) || null;
